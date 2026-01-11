66dab992e941bdd31188f78a87d0333ada5bb3d1
Pedziwiatrr <michalozaurus@gmail.com>
2026-01-11 21:52:43 +0100

    feat: implement A/B testing foundations with model's precision and recall testing

--- ab_test.py
+++ ab_test.py
@@ -1,19 +1,45 @@
+import pickle
+
+from sklearn.metrics import precision_score, recall_score
 from sklearn.model_selection import train_test_split
 
 import utilities as siemionek_kacper
 
 
 def run_ab_tests():
-    print("=" * 10 + "[A/B TESTING]" + "=" * 10)
     listings, calendar, sessions = siemionek_kacper.load_data()
     df = siemionek_kacper.prepare_data(listings, calendar, sessions)
 
     df = siemionek_kacper.create_labels_advanced(df)
 
-    group_a, group_b = train_test_split(df, test_size=0.5, random_state=67)
-    print(
-        f"Group A (control group) length: {len(group_a)}\n Group B (experiment group) length: {len(group_a)}"
-    )
+    with open("model_target.pkl", "rb") as fh:
+        model = pickle.load(fh)
+
+    group_ctrl, group_exp = train_test_split(df, test_size=0.5, random_state=67)
+
+    features = [
+        "number_of_reviews",
+        "review_scores_rating",
+        "host_is_superhost_int",
+        "availability_365",
+        "avg_price_calendar",
+        "std_price_calendar",
+        "session_count",
+        "price_vs_neighbourhood",
+        "room_type",
+    ]
+
+    X_exp = group_exp[features]
+    y_true = group_exp["is_suspicious"]
+
+    y_pred = model.predict(X_exp)
+
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+
+    print("-" * 10 + "RESULTS" + "-" * 10)
+    print(f"Precision: {precision:.2%}")
+    print(f"Recall: {recall:.2%}")
 
 
 if __name__ == "__main__":
